faf2f5909d790d5fda2054b0dbe6bdae28ffd21e
KevinWasTak3n <kcheng446@gmail.com>
2022-02-18 17:57:53 -0800

    Exercises 3-5

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,6 +5,10 @@ using TMPro;
 
 public class BallController : MonoBehaviour
 {
+    public float updateCounter;
+    public float fixedUpdateCounter;
+    private MeshRenderer mr;
+
     public TMP_Text updateText;
     public TMP_Text fixedUpdateText;
     //Declare and intialize updateCounter and fixedUpdateCounter here
@@ -15,6 +19,7 @@ public class BallController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        mr = GetComponent<MeshRenderer>();
         //Set mr to get MeshRenderer component here
         
     }
@@ -22,13 +27,13 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update(){
         //disable MeshRenderer component below
-        
+        mr.enabled = false;
         //increment updateCounter by 1 below
-        
+        updateCounter++;
         //END OF CODE
         
         //Uncomment line below when update function is completed
-        //updateText.text = "Update Counter: " + updateCounter.ToString();
+        updateText.text = "Update Counter: " + updateCounter.ToString();
         
     }
     
@@ -36,8 +41,8 @@ public class BallController : MonoBehaviour
     void FixedUpdate()
     {
         //increment fixedUpdateCounter by 1 and update fixedUpdateText below
-
-        
+        fixedUpdateCounter++;
+        fixedUpdateText.text = "Fixed Update Counter: " + fixedUpdateCounter.ToString();
         //END OF CODE
     }
 }
--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -7,25 +7,39 @@ public class CubeController : MonoBehaviour
 {
     public TMP_Text scoreText;
     //DECLARE VARIABLES HERE
-    
+    private int currentNum;
+    public Vector3 position;
+
+    public float health;
+    public int score;
 
     // Start is called before the first frame update
     void Start()
     {
-        //INITIALIZE CURRENTNUM HERE
+        health = 100;
+        score = 0;
+        currentNum = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //VVV how you get input
+        if(Input.GetKey("space"))
+        {
+            score++;
+            health -= 0.001f;
+        }
+
         //INCREMENT AND PRINT CURRENTNUM HERE 
 
         //Uncomment the code below and complete 
         //the if statement to check if Mouse0 is clicked 
-        // if ()
-        // {
-        //     score++;
-        //     scoreText.text = "Score: " + score.ToString();
-        // }
+         if (Input.GetKey(KeyCode.Mouse0))
+         {
+             score++;
+             scoreText.text = "Score: " + score.ToString();
+            print(score);
+        }
     }
 }
